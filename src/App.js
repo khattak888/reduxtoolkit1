ad1cad974675e5a69202188771b04f7bfea81b53
khattak888 <bilalktk868@gmail.com>
2022-06-14 00:00:07 -0700

    23

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './store/counterSlice';
 function App() {
+ const counter  =useSelector(state => state.counter.value);
+ const dispatch = useDispatch();
+
+
+  // const [counter , setCounter ] = useState(0);
+ 
+  const incrementCounter = () => {
+   dispatch(increment())
+  }
+  const decrementCounter = () => {
+    dispatch(decrement())
+  }
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count: {counter}</h1>
+      <button onClick={incrementCounter}>increment</button>
+      <button onClick={decrementCounter}>decrement</button>
+      <button onClick={()=>{dispatch(incrementByAmount(20))}}>incrementByAmount</button>
     </div>
+    </>
   );
 }
 
